951d97e213f955afcc8968d9944b0ed3ab4b04e4
Sumeet Bachchas <sumeetfullstackdeveloper@gmail.com>
2021-11-10 01:12:17 +0100

    Added Swagger UI for documentation

--- src/main/java/com/app/vitamin/controller/StockInvestmentController.java
+++ src/main/java/com/app/vitamin/controller/StockInvestmentController.java
@@ -1,7 +1,12 @@
 package com.app.vitamin.controller;
 
+import com.app.vitamin.domain.PortfolioRequest;
 import com.app.vitamin.domain.RiskLevelRequest;
+import com.app.vitamin.domain.StockDetails;
 import com.app.vitamin.model.PortfolioEntity;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import java.util.Set;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users/me/investment-portfolio")
 public interface StockInvestmentController {
 
+  @ApiOperation(value = "Fetch Portfolio matching Risk Level")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+      @ApiResponse(code = 500, message = "Failure")})
   @PostMapping(value = "/")
   ResponseEntity<Set<PortfolioEntity>> getPortfolioForRisk(
       @RequestBody RiskLevelRequest riskLevel);
+
+
+  @ApiOperation(value = "Fetch Portfolio matching Risk Level")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+      @ApiResponse(code = 500, message = "Failure")})
+  @PostMapping(value = "/current-value")
+  ResponseEntity<List<StockDetails>> getCurrentPortfolio(
+      @RequestBody PortfolioRequest portfolioRequest);
 
 }
